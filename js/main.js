dff6792be7b6f11c924632bb50f70beb27aa388c
RomanHrytsko <romangritsko@gmail.com>
2021-01-25 15:57:57 +0200

    add burger menu

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let btnMenu = document.getElementById('button')
+let menu = document.getElementById('menu')
+btnMenu.addEventListener('click', () =>{
+    menu.classList.toggle('show-menu')
+})
+
+console.log(menu.classList);
+
+
